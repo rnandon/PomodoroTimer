a0801475272450c941de984d7ca8930959c9fa11
Ryan <ryan@ryannd.com>
2020-11-10 13:24:50 -0600

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace PomodoroTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDownSeconds = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMinutes = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownHours = new System.Windows.Forms.NumericUpDown();
+            this.Hours = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pauseTimer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 82);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDownSeconds
+            // 
+            this.numericUpDownSeconds.AccessibleDescription = "secondsValue";
+            this.numericUpDownSeconds.AccessibleName = "secondsValue";
+            this.numericUpDownSeconds.Location = new System.Drawing.Point(145, 31);
+            this.numericUpDownSeconds.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDownSeconds.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownSeconds.Name = "numericUpDownSeconds";
+            this.numericUpDownSeconds.Size = new System.Drawing.Size(44, 22);
+            this.numericUpDownSeconds.TabIndex = 2;
+            this.numericUpDownSeconds.ValueChanged += new System.EventHandler(this.numericUpDownSeconds_ValueChanged);
+            // 
+            // numericUpDownMinutes
+            // 
+            this.numericUpDownMinutes.Location = new System.Drawing.Point(92, 31);
+            this.numericUpDownMinutes.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDownMinutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDownMinutes.Name = "numericUpDownMinutes";
+            this.numericUpDownMinutes.Size = new System.Drawing.Size(45, 22);
+            this.numericUpDownMinutes.TabIndex = 3;
+            this.numericUpDownMinutes.ValueChanged += new System.EventHandler(this.numericUpDownMinutes_ValueChanged);
+            // 
+            // numericUpDownHours
+            // 
+            this.numericUpDownHours.Location = new System.Drawing.Point(13, 31);
+            this.numericUpDownHours.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDownHours.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDownHours.Name = "numericUpDownHours";
+            this.numericUpDownHours.Size = new System.Drawing.Size(71, 22);
+            this.numericUpDownHours.TabIndex = 4;
+            this.numericUpDownHours.ValueChanged += new System.EventHandler(this.numericUpDownHours_ValueChanged);
+            // 
+            // Hours
+            // 
+            this.Hours.AccessibleDescription = "Hours";
+            this.Hours.AccessibleName = "Hours";
+            this.Hours.AutoSize = true;
+            this.Hours.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hours.Location = new System.Drawing.Point(10, 7);
+            this.Hours.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Hours.Name = "Hours";
+            this.Hours.Size = new System.Drawing.Size(35, 13);
+            this.Hours.TabIndex = 5;
+            this.Hours.Text = "Hours";
+            this.Hours.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Minutes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Seconds";
+            // 
+            // pauseTimer
+            // 
+            this.pauseTimer.AccessibleDescription = "pauseTimer";
+            this.pauseTimer.AccessibleName = "pauseTimer";
+            this.pauseTimer.Location = new System.Drawing.Point(129, 82);
+            this.pauseTimer.Margin = new System.Windows.Forms.Padding(4);
+            this.pauseTimer.Name = "pauseTimer";
+            this.pauseTimer.Size = new System.Drawing.Size(115, 55);
+            this.pauseTimer.TabIndex = 8;
+            this.pauseTimer.Text = "Pause Timer";
+            this.pauseTimer.UseVisualStyleBackColor = true;
+            this.pauseTimer.Click += new System.EventHandler(this.pauseTimer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(261, 149);
+            this.Controls.Add(this.pauseTimer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Hours);
+            this.Controls.Add(this.numericUpDownHours);
+            this.Controls.Add(this.numericUpDownMinutes);
+            this.Controls.Add(this.numericUpDownSeconds);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDownSeconds;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinutes;
+        private System.Windows.Forms.NumericUpDown numericUpDownHours;
+        private System.Windows.Forms.Label Hours;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button pauseTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace PomodoroTimer
+{
+    public partial class Form1 : Form
+    {
+        private static System.Timers.Timer aTimer;
+        static bool stop = false;
+        static TimeValues timeValue = new TimeValues();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDownSeconds_ValueChanged(object sender, EventArgs e)
+        {
+            timeValue.Seconds = numericUpDownSeconds.Value;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timeValue.Seconds == 0 && timeValue.Minutes == 0 && timeValue.Hours == 0)
+            {
+                string messageError = "Invalid time selection. Please use the boxes to create a valid time input.";
+                string captionError = "No Time Input";
+                MessageBoxButtons buttonsError = MessageBoxButtons.OK;
+
+                MessageBox.Show(messageError, captionError, buttonsError);
+                return;
+            }
+            if (timeValue.TimeRemaining <= 0)
+            {
+                timeValue.TimeRemaining = timeValue.Seconds;
+                timeValue.TimeRemaining += timeValue.Minutes * 60;
+                timeValue.TimeRemaining += timeValue.Hours * 3600;
+                timeValue.TimeRemaining *= 1000;
+                timeValue.InitialTime = timeValue.TimeRemaining;
+                timeValue.NextStepThreshold = timeValue.InitialTime / 100;
+            }
+
+            stop = false;
+            aTimer = new System.Timers.Timer(100);
+            aTimer.Enabled = true;
+            aTimer.AutoReset = true;
+            aTimer.Elapsed += OnTimedEvent;
+        }
+
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            timeValue.TimeRemaining -= 100;
+
+            if (timeValue.TimeRemaining < 0)
+            {
+                aTimer.Stop();
+                string messageEnd = "YOUR TIMER IS GOING OFF!!!";
+                string captionEnd = "Timer Ended";
+                MessageBoxButtons buttonsEnd = MessageBoxButtons.OK;
+
+                MessageBox.Show(messageEnd, captionEnd, buttonsEnd);
+            } 
+            else if (stop)
+            {
+                aTimer.Stop();
+                string messageStop = "Timer is paused.";
+                string captionStop = "Timer Paused";
+                MessageBoxButtons buttonsStop = MessageBoxButtons.OK;
+                MessageBox.Show(messageStop, captionStop, buttonsStop);
+            }
+        }
+
+        private void numericUpDownMinutes_ValueChanged(object sender, EventArgs e)
+        {
+            timeValue.Minutes = numericUpDownMinutes.Value;
+        }
+
+        private void numericUpDownHours_ValueChanged(object sender, EventArgs e)
+        {
+            timeValue.Hours = numericUpDownHours.Value;
+        }
+
+        private void pauseTimer_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    public class TimeValues
+    {
+        public decimal Seconds
+        { get; set; }
+
+        public decimal Minutes
+        { get; set; }
+
+        public decimal Hours
+        { get; set; }
+
+        public decimal TimeRemaining
+        { get; set; }
+
+        public decimal InitialTime
+        { get; set; }
+
+        public decimal NextStepThreshold
+        { get; set; }
+
+        public int CurrentStep
+        { get; set; }
+
+        public TimeValues()
+        {
+            this.Seconds = 0;
+            this.Minutes = 0;
+            this.Hours = 0;
+            this.TimeRemaining = 0;
+            this.InitialTime = 0;
+            this.NextStepThreshold = 0;
+            this.CurrentStep = 0;
+        }
+    }
+}
